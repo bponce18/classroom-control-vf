e5fcf60e113fb7821935e181d9678fae293e838d
bponce18 <bradley.ponce@state.ca.gov>
2016-08-23 15:25:46 -0700

    Update site.pp
    Added class

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,5 +42,6 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  notify { "Hello, my name is Frodo ${::hostname}": }
+  notify { "Hello, my name is ${::hostname}": }
+  include examples::fundamentals
 }
