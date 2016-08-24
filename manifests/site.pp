dffb59b0082cfc07cb3307dd1257d2ec6d6c379e
bponce18 <bradley.ponce@state.ca.gov>
2016-08-24 11:37:03 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,7 +44,7 @@ node default {
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}": }
   include examples::fundamentals
-  
+  include users
   exec { "cowsay 'Welcome to ${::fqdn}' > /etc/motd":
     path    => '/usr/local/bin',
     creates => '/etc/motd',
