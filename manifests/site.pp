d67c92784f37b3a9cc4cd218abfdd547015efbda
bponce18 <bradley.ponce@state.ca.gov>
2016-08-24 15:53:43 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -47,6 +47,7 @@ node default {
   include users
   include skeleton
   include memcached
+  include nginx
   exec { "cowsay 'Welcome to ${::fqdn}' > /etc/motd":
     path    => '/usr/local/bin',
     creates => '/etc/motd',
